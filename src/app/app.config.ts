adc8372875dd96da54f52a3b78ba132569b15732
Hazem Alyaari <23160037@su.edu.ye>
2026-08-05 00:23:03 +0300

    refactor: add theme initialization to app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { ThemeService } from './services/theme.service';
+
+function initializeTheme(themeService: ThemeService) {
+  return () => themeService.init();
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeTheme,
+      deps: [ThemeService],
+      multi: true,
+    },
+  ],
 };
